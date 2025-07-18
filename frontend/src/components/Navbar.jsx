d1406a3b8a444886b59b38048a00efb4b4f8f544
Ibad Khan <ibadkhansherani@gmail.com>
2025-07-18 10:26:48 -0700

    navbar updatee

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {assets} from '../assets/assets';
-import { NavLink } from 'react-router-dom';
+import { NavLink,Link } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -31,10 +31,25 @@ function Navbar() {
             <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
             <div className='group relative'>
                 <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
-                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'></div>
+                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                        <p className='cursor-pointer hover:text-black'>My Profiles</p>
+                        <p className='cursor-pointer hover:text-black'>Orders</p>
+                        <p className='cursor-pointer hover:text-black'>Logout</p>
+                    </div>
+
+
+                </div>
 
 
             </div>
+      <Link to='/cart' className='relative'>
+      <img src={assets.cart_icon}  className='w-5 min-w-5 cursor-pointer' alt="" />
+      <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square  rounded-full text-[8px] '>0</p>
+
+      </Link>
+
+
         </div>
 
 
