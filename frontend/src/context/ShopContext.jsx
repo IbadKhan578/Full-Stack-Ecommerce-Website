024bcdaa0f7e4c9921070646e6773f3653d28f2f
Ibad Khan <ibadkhansherani@gmail.com>
2025-07-20 03:30:05 -0700

    Added shop context

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+import { products } from "../assets/assets";
+export const shopContext = createContext();
+
+const shopcontextProvider = (props) => {
+  const currency = "$";
+  const delivery_fee = 250;
+
+  let value = {
+    products,
+    currency,
+    delivery_fee,
+  };
+
+  return (
+    <shopContext.Provider value={value}>
+        {props.children}
+     </shopContext.Provider>
+  );
+};
+
+export default shopcontextProvider;
