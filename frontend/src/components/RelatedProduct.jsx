415381684059af38d52342acb22e546134632d7a
Ibad Khan <ibadkhansherani@gmail.com>
2025-07-22 08:25:00 -0700

    Added product page and relatedProduct Component

---
+++ frontend/src/components/RelatedProduct.jsx
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { shopContext } from '../context/ShopContext'
+import SectionTitle from './SectionTitle';
+import Productitem from './Productitem';
+import { useNavigate } from 'react-router-dom';
+
+function RelatedProduct({category, subCategory}) {
+    const {products} = useContext(shopContext);
+    const [related,setRelated] = useState([]);
+        useEffect(()=>{
+        if (products.length>0) {
+            let productCopy = products.slice();
+            productCopy = productCopy.filter((item)=> category===item.category)
+            productCopy = productCopy.filter((item)=> subCategory===item.subCategory)
+            setRelated(productCopy.slice(0,5))
+            console.log(productCopy.slice(0,5));
+        }
+
+
+    },[products])
+
+
+  return (
+ <div className='my-24'>
+       <div className=' py-2 text-3xl  text-center'>
+            <SectionTitle  text1={'RELATED'} text2={'PRODUCTS'} />
+    </div>
+  <div className='grid grid-cols-2 md:grid-cols-4 sm:grid-cols-3  lg:grid-cols-5 gap-4 gap-y-6'>
+      {
+        related.map((item,index)=>(
+            <Productitem  key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+        ))
+    }
+  </div>
+
+ </div>
+  )
+}
+
+export default RelatedProduct
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { PrefetchPageLinks, useParams } from "react-router-dom";
+import { assets, products } from "../assets/assets";
+import { shopContext } from "../context/ShopContext";
+import RelatedProduct from "../components/RelatedProduct";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const { productId } = useParams(); // fetch the product id  id from the url
+  const { products, currency } = useContext(shopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
 
-export default Product
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        console.log(productData);
+        setImage(item.image[0]);
+        return null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+   window.scrollTo({ top: 0, behavior: 'smooth' }); // ✅ scroll to top
+
+
+  }, [productId, products]);
+
+  return productData ? (
+    <div className="border-t-2 border-gray-300 pt-10 transition-opacity ease-in duration-500 opacity-100 ">
+      {/* product data */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* product images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex   sm:flex-col overflow-x-auto  sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer "
+                alt=""
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img className="w-full h-auto" src={image} alt="" />
+          </div>
+        </div>
+
+        {/* product info */}
+        <div className="flex-1">
+          <h1 className="font-medium  text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2 ">
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_icon} alt="" className="w-3 5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className="pl-3">(122)</p>
+          </div>
+          <p className="mt-5 font-medium text-2xl">
+            {currency} {productData.price}
+          </p>
+          <p className="text-gray-500 mt-5 md:w-4/5">
+            {productData.description}
+          </p>
+
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={` border py-2 px-4 bg-gray-100 cursor-pointer ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm cursor-pointer active:bg-gray-700">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 text-gray-300 sm:w-4/5" />
+          <div className="flex text-sm text-gray-500 mt-5 gap-1 flex-col">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Description and review section */}
+      <div className="mt-20">
+        <div className="flex ">
+          <b className="border border-gray-300 py-3 px-5 text-sm">
+            Description
+          </b>
+          <p className="border border-gray-300 py-3 px-5 text-sm">
+            Reviews (122)
+          </p>
+        </div>
+        <div className="border border-gray-300 py-4 px-6 text-sm sm:text-base text-gray-500 flex">
+          <p>
+            An e-commerce website is an online platform that facilitates the
+            buying and selling of products or services over the internet. It
+            serves as a virtual marketplace where businesses and individuals can
+            showcase their products, interact with customers, and conduct
+            transactions without the need for a physical presence. E-commerce
+            websites have gained immense popularity due to their convenience,
+            accessibility, and the global reach they offer. <br /> <br />{" "}
+            E-commerce websites typically display products or services along
+            with detailed descriptions, images, prices, and any available
+            variations (e.g., sizes, colors). Each product usually has its own
+            dedicated page with relevant information.
+          </p>
+        </div>
+      </div>
+       {/* related product section */}
+       <RelatedProduct  category={productData.category} subCategory={productData.subCategory}/>
+
+
+    </div>
+  ) : (
+    <div className="opacity-0">;</div>
+  );
+};
+
+export default Product;
