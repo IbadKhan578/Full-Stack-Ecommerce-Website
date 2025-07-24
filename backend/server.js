b6e12a392a15f7517e6239194565487ba75d3494
Ibad Khan <ibadkhansherani@gmail.com>
2025-07-24 01:55:22 -0700

    added db connection

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,21 @@
 import express from 'express';
-import cors from 'cors'
-import 'dotenv/config'
+import cors from 'cors';
+import 'dotenv/config';
+import connectDb from './config/mongodb.js';
+
+// app config
+const app = express();
+const port = process.env.PORT || 8080;
+connectDb();
+
+ // middleware 
+app.use(cors());  // to run client and server on same ip
+app.use(express.json());  // to parse json
+
+app.get('/', (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running at port: ${port}`);
+});
