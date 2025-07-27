08c5e7d8a513c99b55d2e432060757e21625793a
Ibad Khan <ibadkhansherani@gmail.com>
2025-07-27 01:02:06 -0700

    definded model , controller , routes from orderes

---
+++ backend/Controllers/orderController.js
@@ -0,0 +1,37 @@
+
+
+
+//placing order using cod method
+
+
+const placeOrder = async(req,res)=>{
+
+}
+
+
+const placeOrderStripe = async(req,res)=>{
+    
+}
+
+const placeOrderRazorpay = async(req,res)=>{
+    
+}
+
+// All orders for admin panel
+
+const AllOrders = async (req,res)=>{
+
+}
+
+const userOrders = async (req,res)=>{
+
+}
+
+
+// update order status from admin
+
+const updateStatus = async (req,res)=>{
+
+}
+
+export {placeOrder,placeOrderStripe , placeOrderRazorpay, AllOrders , userOrders , updateStatus}
---
+++ backend/Routes/orderRoute.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import adminAuth from '../middleware/adminAuth.js';
+import authUser from '../middleware/Auth.js';
+import {placeOrder,placeOrderStripe , placeOrderRazorpay, AllOrders , userOrders , updateStatus} from '../Controllers/orderController.js';
+
+const OrderRouter = express.Router();
+
+// admin features
+OrderRouter.post('/list',adminAuth, AllOrders)
+OrderRouter.post('/status',adminAuth,updateStatus);
+
+//  payment features
+
+OrderRouter.post('/place',authUser,placeOrder);
+OrderRouter.post('/stripe',authUser,placeOrderStripe);
+OrderRouter.post('/razorpay',authUser,placeOrderRazorpay);
+
+// user feature
+
+OrderRouter.post('/userorders',authUser,userOrders);
+
+
+export default OrderRouter;
